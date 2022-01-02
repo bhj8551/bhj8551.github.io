db2fa7f2f6283384f611c4f257a257a7373dbe61
bae hong jik <63235354+bhj8551@users.noreply.github.com>
2022-01-03 02:17:51 +0900

    Add: PostItem, PostLIst

--- src/components/Main/Introduction.tsx
+++ src/components/Main/Introduction.tsx
@@ -7,7 +7,6 @@ const Background = styled.div`
   background-image: linear-gradient(60deg, #29323c 0%, #485563 100%);
   color: #ffffff;
 `
-
 const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
@@ -16,17 +15,31 @@ const Wrapper = styled.div`
   width: 768px;
   height: 400px;
   margin: 0 auto;
+
+  @media (max-width: 768px) {
+    width: 100%;
+    height: 300px;
+    padding: 0 20px;
+  }
 `
 
 const SubTitle = styled.div`
   font-size: 20px;
   font-weight: 400;
+
+  @media (max-width: 768px) {
+    font-size: 15px;
+  }
 `
 
 const Title = styled.div`
   margin-top: 5px;
   font-size: 35px;
   font-weight: 700;
+
+  @media (max-width: 768px) {
+    font-size: 25px;
+  }
 `
 
 const Introduction: FunctionComponent = function () {
--- src/components/Main/ProfileImage.tsx
+++ src/components/Main/ProfileImage.tsx
@@ -4,11 +4,17 @@ import myImage from '../../../contents/assets/profile.png'
 
 // 자신이 원하는 프로필 이미지 링크로 설정해주세요.
 
+
 const ProfileImageWrapper = styled.img`
   width: 120px;
   height: 120px;
   margin-bottom: 30px;
   border-radius: 50%;
+
+  @media (max-width: 768px) {
+    width: 80px;
+    height: 80px;
+  }
 `
 
 const ProfileImage: FunctionComponent = function () {
